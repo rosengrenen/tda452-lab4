4bbc3d686daf57e75a882f531e7cfb62cb55fc02
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-12-15 15:19:04 +0100

    Generalize A

--- Expr.hs
+++ Expr.hs
@@ -1,9 +1,26 @@
+module Expr where
+
 data Expr = Num Double
   | Add Expr Expr
-  | Mul Expr Expr 
+  | Mul Expr  Expr 
   | Cos Expr
   | Sin Expr
+  | Op Expr Operand Expr
+  | Func Name Expr
   | Var
+type Name = String
+type Operand = Char
+data Opr = Opr Char (Double -> Double -> Double)
+
+operators :: [(Char, (Double -> Double -> Double))]
+operators = [
+  ('+', (+)),
+  ('*', (*)) ]
+
+functions :: [(String, (Double -> Double))]
+functions = [
+  ("cos", Prelude.cos),
+  ("sin", Prelude.sin) ]
 
 x :: Expr
 x = Var
