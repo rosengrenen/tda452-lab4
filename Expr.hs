22706d608572d97a00c298052ba23dd237b1f065
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-12-15 16:14:21 +0100

    B

--- Expr.hs
+++ Expr.hs
@@ -1,4 +1,7 @@
 module Expr where
+import Data.Maybe
+
+-- A --------------------------------------------------------------------------
 
 data Expr = Num Double
   | Op Expr Operand Expr
@@ -6,13 +9,13 @@ data Expr = Num Double
   | Var
   deriving Show
 type Name = String
-type Operand = Char
+type Operand = String
 data Opr = Opr Char (Double -> Double -> Double)
 
-operators :: [(Char, (Double -> Double -> Double))]
+operators :: [(String, (Integer, (Double -> Double -> Double)))]
 operators = [
-  ('+', (+)),
-  ('*', (*)) ]
+  ("+", (0, (+))),
+  ("*", (1, (*))) ]
 
 functions :: [(String, (Double -> Double))]
 functions = [
@@ -24,8 +27,24 @@ x = Var
 num :: Double -> Expr
 num value = Num value
 add,mul :: Expr -> Expr -> Expr
-add expr1 expr2 = Op expr1 '+' expr2
-mul expr1 expr2 = Op expr1 '*' expr2
+add expr1 expr2 = Op expr1 "+" expr2
+mul expr1 expr2 = Op expr1 "*" expr2
 sin,cos :: Expr -> Expr
 sin expr = Func "sin" expr
 cos expr = Func "cos" expr
+
+
+-- B --------------------------------------------------------------------------
+
+showExpr :: Expr -> String
+showExpr expr = showExpr' expr (-1)
+
+showExpr' :: Expr -> Integer -> String
+showExpr' (Num num)                _ = show num
+showExpr' (Op expr1 operand expr2) p 
+  | p > precedence = "(" ++ showExpr' expr1 precedence ++ " " ++ operand ++ " " ++ showExpr' expr2 precedence ++ ")"
+  | otherwise      = showExpr' expr1 precedence ++ " " ++ operand ++ " " ++ showExpr' expr2 precedence
+  where
+    (precedence, _) = fromJust $ lookup operand operators 
+showExpr' (Func name expr)         _ = name ++ showExpr' expr 999
+showExpr' Var                      _ = "x"
