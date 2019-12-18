7b26d2483ce6762768a63487085fae61af05e08c
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-12-18 16:44:40 +0100

    J

--- Calculator.hs
+++ Calculator.hs
@@ -1,32 +1,36 @@
--- This module is a starting point for implementing the Graph Drawing
 -- Calculator as described in Part II of the Standard Lab. You can use this
--- directly, or just study it as an example of how to use threepenny-gui.
+-- This module is a starting point for implementing the Graph Drawing
 import Data.Maybe
-import Expr
+-- directly, or just study it as an example of how to use threepenny-gui.
 import ThreepennyPages
+import Expr
+
 import Graphics.UI.Threepenny.Core as UI
 import qualified Graphics.UI.Threepenny as UI
 
-canWidth,canHeight :: Num a => a
+canWidth, canHeight :: Num a => a
 canWidth  = 300
 canHeight = 300
 
 main :: IO ()
 main = startGUI defaultConfig setup
-
 setup :: Window -> UI ()
 setup window =
   do -- Create them user interface elements
-     canvas  <- mkCanvas canWidth canHeight   -- The drawing area
-     fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
-     input   <- mkInput 20 "x"                -- The formula input
-     draw    <- mkButton "Draw graph"         -- The draw button
+     canvas     <- mkCanvas canWidth canHeight   -- The drawing area
+     input      <- mkInput 20 "x"                -- The formula input
+     fx         <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
+     draw       <- mkButton "Draw graph"         -- The draw button
+     z          <- mkHTML "<i>Zoom</i>="  -- The text "f(x)="
+     zoomButton <- mkButton "Zoom"         -- The draw button
+     zoomInput  <- mkInput 20 "0.04"                -- The formula input
        -- The markup "<i>...</i>" means that the text inside should be rendered
        -- in italics.
 
      -- Add the user interface elements to the page, creating a specific layout
      formula <- row [pure fx,pure input]
-     getBody window #+ [column [pure canvas,pure formula,pure draw]]
+     zoom <- row [pure z, pure zoomInput]
+     getBody window #+ [column [pure canvas,pure formula,pure draw, pure zoom, pure zoomButton]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
@@ -34,14 +38,17 @@ setup window =
      pure input # set style [("fontSize","14pt")]
 
      -- Interaction (install event handlers)
-     on UI.click     draw  $ \ _ -> readAndDraw input canvas
-     on valueChange' input $ \ _ -> readAndDraw input canvas
+     on UI.click     draw  $ \ _ -> readAndDraw input zoomInput canvas
+     on valueChange' input $ \ _ -> readAndDraw input zoomInput canvas
+     on UI.click     zoomButton  $ \ _ -> readAndDraw input zoomInput canvas
+     on valueChange' zoomInput $ \ _ -> readAndDraw input zoomInput canvas
 
 
-readAndDraw :: Element -> Canvas -> UI ()
-readAndDraw input canvas =
+readAndDraw :: Element -> Element -> Canvas -> UI ()
+readAndDraw input zoomInput canvas =
   do -- Get the current formula (a String) from the input element
      formula <- get value input
+     zoom    <- get value zoomInput
      let expression = fromMaybe (Num 0) (readExpr formula)
      -- Clear the canvas
      clearCanvas canvas
@@ -49,7 +56,7 @@ readAndDraw input canvas =
      -- It should be replaced with code that draws the graph of the function.
      set UI.fillStyle (UI.solidColor (UI.RGB 0 0 0)) (pure canvas)
      UI.fillText formula (10,canHeight/2) canvas
-     path "blue" (points expression 0.04 (canWidth, canHeight)) canvas
+     path "blue" (points expression (read zoom) (canWidth, canHeight)) canvas
 
 -- H --------------------------------------------------------------------------
 
