7850b3551ac0f827d928d4b5325cfc2ba460536c
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-12-27 16:21:46 +0100

    GENERALIZE THE FUCK OUT OF IT

--- Expr.hs
+++ Expr.hs
@@ -7,113 +7,103 @@ import Test.QuickCheck
 -- A --------------------------------------------------------------------------
 
 data Expr = Num Double
-  | Add Expr Expr
-  | Mul Expr Expr
-  | Sin Expr
-  | Cos Expr
+  | Operation Operator Expr Expr
+  | Function FunctionName Expr
   | Var
   deriving (Eq, Show)
+
+type Operator = String
+type FunctionName = String
   
--- type Name = String
--- type Operand = Char
+operators :: [(String, (Integer, Double -> Double -> Double))]
+operators = [
+  ("+", (0, (+))),
+  ("*", (1, (*))) ]
 
--- operators :: [(Char, (Integer, Double -> Double -> Double))]
--- operators = [
---   ('+', (0, (+))),
---   ('-', (0, (-))),
---   ('*', (1, (*))),
---   ('/', (1, (/))) ]
-
--- functions :: [(String, Double -> Double)]
--- functions = [
---   ("cos", Prelude.cos),
---   ("sin", Prelude.sin) ]
+functions :: [(String, Double -> Double)]
+functions = [
+  ("cos", Prelude.cos),
+  ("sin", Prelude.sin) ]
 
 x :: Expr
 x = Var
 num :: Double -> Expr
 num = Num
 add,mul :: Expr -> Expr -> Expr
-add = Add
-mul = Mul
+add = Operation "+"
+mul = Operation "*"
 sin,cos :: Expr -> Expr
-sin = Sin
-cos = Cos
+sin = Function "sin"
+cos = Function "cos"
 
 -- B --------------------------------------------------------------------------
 
 showExpr :: Expr -> String
-showExpr Var = "x"
-showExpr (Num n) = show n
-showExpr (Add e e') = showExpr e ++ " + " ++ showExpr e'
-showExpr (Mul e e') = showFactor e ++ " * " ++ showFactor e'
-showExpr (Sin e) = "sin" ++ showFunction e
-showExpr (Cos e) = "cos" ++ showFunction e
+showExpr expr = showExpr' expr (-1)
 
-showFactor :: Expr -> String
-showFactor e@(Add _ _) = "(" ++ showExpr e ++ ")"
-showFactor e           = showExpr e
-
-showFunction :: Expr -> String
-showFunction e@(Add _ _) = "(" ++ showExpr e ++ ")"
-showFunction e@(Mul _ _) = "(" ++ showExpr e ++ ")"
-showFunction e           = " " ++ showExpr e
+showExpr' :: Expr -> Integer -> String
+showExpr' (Num num)                 _ = show num
+showExpr' (Operation operator e e') p
+  | p > precedence = "(" ++ showExpr' e precedence ++ " " ++ operator ++ " " ++ showExpr' e' precedence ++ ")"
+  | otherwise      = showExpr' e precedence ++ " " ++ operator ++ " " ++ showExpr' e' precedence
+  where
+    (precedence, _) = fromJust $ lookup operator operators 
+showExpr' (Function name e)         _ = name ++ " " ++ showExpr' e 999
+showExpr' Var                       _ = "x"
 
 -- C --------------------------------------------------------------------------
 
 eval :: Expr -> Double -> Double
-eval Var        var = var
-eval (Num num)  _   = num
-eval (Add e e') var = eval e var + eval e' var
-eval (Mul e e') var = eval e var * eval e' var
-eval (Sin e)    var = Prelude.sin $ eval e var
-eval (Cos e)    var = Prelude.cos $ eval e var
+eval (Num num)                 _ = num
+eval (Operation operator e e') x = eval e x `op` eval e' x
+  where
+    (_, op) = fromJust $ lookup operator operators
+eval (Function name e)         x = func $ eval e x
+  where
+    func = fromJust $ lookup name functions
+eval Var                       x = x
 
 -- D --------------------------------------------------------------------------
 
 readExpr :: String -> Maybe Expr
-readExpr s = maybeExpr $ parse expr (filter (/=' ') s)
+readExpr s = maybeExpr $ parse (expr 0) (filter (/=' ') s)
 
 maybeExpr :: Maybe (Expr, String) -> Maybe Expr
-maybeExpr (Just (e, s)) | s == ""    = Just {-- $assoc --}e
+maybeExpr (Just (e, s)) | s == ""    = Just e
                          | otherwise = Nothing
 maybeExpr Nothing                    = Nothing
 
 number :: Parser Expr
 number = Num . head <$> oneOrMore readsP
 
-
 variable :: Parser Expr
 variable = do char 'x'; return Var
 
-expr :: Parser Expr
-expr = foldl1 Add <$> chain term (char '+')
+string :: String -> Parser String
+string str = traverse char str
 
-term :: Parser Expr
-term = foldl1 Mul <$> chain factor (char '*')
+expr :: Int -> Parser Expr
+expr n
+  | n < length operators = foldl1 (Operation operator) <$> chain (expr (n + 1)) (string operator)
+  | otherwise            = factor
+  where
+    (operator, _) = operators !! n
 
 factor :: Parser Expr
 factor = parentheses <|> func <|> number <|> variable
 
-parseSin :: Parser Expr
-parseSin = do
-  char 's'
-  char 'i'
-  char 'n'
-  Sin <$> factor
-
-parseCos :: Parser Expr
-parseCos = do
-  char 'c'
-  char 'o'
-  char 's'
-  Cos <$> factor
+func' :: Parser String
+func' = foldl1 (<|>) [string $ fst f | f <- functions]
 
 func :: Parser Expr
-func = parseSin <|> parseCos
+func = do
+  name <- func'
+  if isJust $ lookup name functions
+  then Function name <$> factor
+  else failure
 
 parentheses :: Parser Expr
-parentheses = char '(' *> expr <* char ')'
+parentheses = char '(' *> expr 0 <* char ')'
 
 -- E --------------------------------------------------------------------------
 
@@ -127,7 +117,7 @@ arbExpr :: Int -> Gen Expr
 arbExpr s = frequency [(1, rNum 4),(1, rVar),(s, rBin s), (s, rFunc s)]
 
 rNum :: Double -> Gen Expr
-rNum range = {--assoc <$> --}elements (map Num [0..range])
+rNum range = elements (map Num [0..range])
 
 rVar :: Gen Expr
 rVar = elements [Var]
@@ -135,7 +125,7 @@ rVar = elements [Var]
 rBin :: Int -> Gen Expr
 rBin size = do 
   let size' = size `div` 2
-  op <- elements [Mul, Add]
+  op <- elements [Operation $ fst o | o <- operators]
   e <- arbExpr size'
   e' <- arbExpr size'
   return $ op e e'
@@ -143,41 +133,40 @@ rBin size = do
 rFunc :: Int -> Gen Expr
 rFunc size = do 
   let size' = size `div` 2
-  fn <- elements [Sin, Cos]
+  fn <- elements [Function $ fst f | f <- functions]
   e <- arbExpr size'
   return $ fn e
 
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
 
--- F --------------------------------------------------------------------------
+-- -- F --------------------------------------------------------------------------
 
 simplify :: Expr -> Expr
 simplify e | e == simplify' e = e
            | otherwise        = simplify (simplify' e)
 
 simplify' :: Expr -> Expr
-simplify' (Add Var       Var       ) = Mul (Num 2) Var
-simplify' (Add (Num num) (Num num')) = Num (num + num')
-simplify' (Add (Num 0)   e'        ) = e'
-simplify' (Add e         (Num 0)   ) = e
-simplify' (Add e         e'        ) = Add (simplify e) (simplify e')
-simplify' (Mul (Num num) (Num num')) = Num (num * num')
-simplify' (Mul (Num 0)   _         ) = Num 0
-simplify' (Mul _         (Num 0)   ) = Num 0
-simplify' (Mul e         (Num 1)   ) = e
-simplify' (Mul (Num 1)   e         ) = e
-simplify' (Mul e e'                ) = Mul (simplify e) (simplify e')
-simplify' (Sin (Num 0)             ) = Num 0    
-simplify' (Sin e                   ) = Sin (simplify e)
-simplify' (Cos (Num 0)             ) = Num 1    
-simplify' (Cos e                   ) = Cos (simplify e)
-simplify' e                          = e
+simplify' (Operation "+" Var       Var       ) = Operation "*" (Num 2) Var
+simplify' (Operation "+" (Num num) (Num num')) = Num (num + num')
+simplify' (Operation "+" (Num 0)   e'        ) = e'
+simplify' (Operation "+" e         (Num 0)   ) = e
+simplify' (Operation "+" e         e'        ) = Operation "+" (simplify e) (simplify e')
+simplify' (Operation "*" (Num num) (Num num')) = Num (num * num')
+simplify' (Operation "*" (Num 0)   _         ) = Num 0
+simplify' (Operation "*" _         (Num 0)   ) = Num 0
+simplify' (Operation "*" e         (Num 1)   ) = e
+simplify' (Operation "*" (Num 1)   e         ) = e
+simplify' (Operation "*" e e'                ) = Operation "*" (simplify e) (simplify e')
+simplify' e@(Function _ (Num _)              ) = Num $ eval e 0
+simplify' (Function "sin" e                  ) = Function "sin" (simplify e)
+simplify' (Function "cos" e                  ) = Function "cos" (simplify e)
+simplify' e                                    = e
 
 prop_simplify :: Expr -> Bool
 prop_simplify e = doubleEq 0.0001 (eval e 1) (eval (simplify e) 1) 
 
--- G --------------------------------------------------------------------------
+-- -- G --------------------------------------------------------------------------
 
 differentiate :: Expr -> Expr
 differentiate e = simplify (differentiate' (simplify e))
@@ -185,7 +174,7 @@ differentiate e = simplify (differentiate' (simplify e))
 differentiate' :: Expr -> Expr
 differentiate' Var        = Num 1
 differentiate' (Num _)    = Num 0
-differentiate' (Add e e') = Add (differentiate' e) (differentiate' e')
-differentiate' (Mul e e') = Add (Mul (differentiate' e) e') (Mul e (differentiate' e'))
-differentiate' (Cos e)    = Mul (Num (-1)) (Mul (Sin e) (differentiate' e))
-differentiate' (Sin e)    = Mul (Cos e) (differentiate' e)
+differentiate' (Operation "+" e e') = Operation "+" (differentiate' e) (differentiate' e')
+differentiate' (Operation "*" e e') = Operation "+" (Operation "*" (differentiate' e) e') (Operation "*" e (differentiate' e'))
+differentiate' (Function "cos" e)   = Operation "*" (Num (-1)) (Operation "*" (Function "sin" e) (differentiate' e))
+differentiate' (Function "sin" e)   = Operation "*" (Function "cos" e) (differentiate' e)
